b396045d7ca399e645596095c6c7286a683c9081
AmanLucifer <mr.amanport@gmail.com>
2024-07-29 00:14:17 +0530

    update

---
+++ Neev/02_datatye.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+// already exucute this code 
+
+
+// console.log
+//     ("drinkcode")
+
+
+// console.log(4543 +
+//     21);
+
+//ode readability should be high
+
+console.log("drinkcode");
+console.log(4543 + 21);
+
+// data type  [Primitive]
+
+// string  => ""
+// number  => 2 is to the power 53
+//Bigint   => above   2 is to the power 53
+//Boolean  => true/false
+//null     
+//undefined  
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
