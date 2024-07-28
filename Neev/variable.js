7e6b4cb8a81cb7800fec5800c17cf853326320f8
AmanLucifer <mr.amanport@gmail.com>
2024-07-28 22:55:29 +0530

    LetVarConst

---
+++ Neev/variable.js
@@ -0,0 +1,19 @@
+
+const acountId = 2345432
+let acountEmail = "drinkcode@gmail.com"
+var userAddresss = "Bihar"
+
+let userPincode;
+
+// acountId = 787878
+
+acountEmail = "aman@drinkcode.com"
+userAddresss = "Patna"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// console.log(acountId);
+console.table([acountId, acountEmail, userAddresss, userPincode])
